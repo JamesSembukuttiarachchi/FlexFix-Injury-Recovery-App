a05357442a1bc002f18379ffea485dc3d04f3c93
RakinduM <rakindumarambe17@gmail.com>
2024-10-07 04:19:56 +0530

    Add the SignUp

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,118 @@
-import { View, Text, ImageBackground } from "react-native";
-import React from "react";
-import beachImage from "@/assets/beach.webp";
-import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
-import CustomButton from "@/components/CustomButton";
-import { useRouter } from "expo-router";
-//import AppGradient from "@/components/AppGradient";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import CustomTextInput from "../components/CustomTextInput";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore"; // Firestore imports
+import { auth, db } from "../firebaseConfig"; // Ensure db is imported from firebaseConfig
 
-const App = () => {
-  const router = useRouter();
+const SignUpScreen: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleSignUp = async () => {
+    if (name === "" || email === "" || phoneNumber === "" || password === "") {
+      Alert.alert("Error", "All fields are required.");
+      return;
+    }
+
+    try {
+      // Create user in Firebase Authentication
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      console.log("User created successfully:", user);
+
+      // Store user information in Firestore
+      await setDoc(doc(db, "users", user.uid), {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        createdAt: new Date(),
+      });
+
+      Alert.alert("Success", "Account created and user data saved.");
+    } catch (error) {
+      console.error("Error creating user or saving data:", error);
+      Alert.alert("Error");
+    }
+  };
+
   return (
-    <View className="flex-1">
-      <ImageBackground
-        source={beachImage}
-        resizeMode="cover"
-        className="flex-1"
+    <View className="flex-1 justify-center px-5 bg-white">
+      <Text className="text-center text-2xl font-bold mb-8">
+        Create an account
+      </Text>
+
+      <CustomTextInput
+        placeholder="Enter Your Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <CustomTextInput
+        placeholder="Enter Your Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <CustomTextInput
+        placeholder="Enter Your Phone Number"
+        keyboardType="phone-pad"
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+      />
+      <View className="mb-4">
+        <View className="relative">
+          <CustomTextInput
+            placeholder="Enter Your Password"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity
+            onPress={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-4"
+          >
+            <Ionicons
+              name={showPassword ? "eye-off" : "eye"}
+              size={24}
+              color="gray"
+              className=""
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        className="bg-orange-500 py-4 rounded-lg items-center mb-6"
+        onPress={handleSignUp}
       >
-        <LinearGradient className="flex-1" colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+        <Text className="text-white font-bold text-base">Sign Up</Text>
+      </TouchableOpacity>
 
-            <SafeAreaView className="flex-1 px-1 justify-between">
-              <View>
-                <Text className="text-center text-white font-bold text-4xl">
-                  Simple Meditation
-                </Text>
-                <Text className="text-center text-white mt-3 text-2xl">
-                  Simplifying Meditation for Everyone
-                </Text>
-              </View>
-              <View>
-                <CustomButton
-                  onPress={() => router.push("/nature-meditate")}
-                  title="Get Started"
-                />
-              </View>
-              <StatusBar style="light" />
-            </SafeAreaView>
+      <View className="flex-row items-center mb-6">
+        <View className="flex-1 h-[1px] bg-gray-300" />
+        <Text className="mx-2 text-gray-500">Or With</Text>
+        <View className="flex-1 h-[1px] bg-gray-300" />
+      </View>
 
-        </LinearGradient>
-      </ImageBackground>
+      <TouchableOpacity className="flex-row justify-center items-center border border-gray-300 py-4 rounded-lg mb-6">
+        <FontAwesome name="google" size={24} color="black" />
+        <Text className="ml-2 font-bold text-base">Signup with Google</Text>
+      </TouchableOpacity>
+
+      <Text className="text-center text-gray-500">
+        Already have an account?{" "}
+        <Text className="text-orange-500 font-bold">Login</Text>
+      </Text>
     </View>
   );
 };
 
-export default App;
+export default SignUpScreen;
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,19 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import Constants from 'expo-constants';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.FIREBASE_API_KEY,
-  authDomain: Constants.manifest.extra.FIREBASE_AUTH_DOMAIN,
-  projectId: Constants.manifest.extra.FIREBASE_PROJECT_ID,
-  storageBucket: Constants.manifest.extra.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Constants.manifest.extra.FIREBASE_MESSAGING_SENDER_ID,
-  appId: Constants.manifest.extra.FIREBASE_APP_ID,
-  measurementId: Constants.manifest.extra.FIREBASE_MEASUREMENT_ID
+  apiKey: Constants.manifest.extra.firebaseConfig.apiKey,
+  authDomain: Constants.manifest.extra.firebaseConfig.authDomain,
+  projectId: Constants.manifest.extra.firebaseConfig.projectId,
+  storageBucket: Constants.manifest.extra.firebaseConfig.storageBucket,
+  messagingSenderId: Constants.manifest.extra.firebaseConfig.messagingSenderId,
+  appId: Constants.manifest.extra.firebaseConfig.appId,
+  measurementId: Constants.manifest.extra.firebaseConfig.measurementId,
 };
 
 // Initialize Firebase
