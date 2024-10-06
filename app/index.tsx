0cd19072fff7bf8d3338c3609013f4aad0b28c31
RakinduM <rakindumarambe17@gmail.com>
2024-10-07 01:56:26 +0530

    add Splash Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import beachImage from "@/assets/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
+//import AppGradient from "@/components/AppGradient";
 
-const index = () => {
+const App = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient className="flex-1" colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
 
-export default index
+            <SafeAreaView className="flex-1 px-1 justify-between">
+              <View>
+                <Text className="text-center text-white font-bold text-4xl">
+                  Simple Meditation
+                </Text>
+                <Text className="text-center text-white mt-3 text-2xl">
+                  Simplifying Meditation for Everyone
+                </Text>
+              </View>
+              <View>
+                <CustomButton
+                  onPress={() => router.push("/nature-meditate")}
+                  title="Get Started"
+                />
+              </View>
+              <StatusBar style="light" />
+            </SafeAreaView>
+
+        </LinearGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
